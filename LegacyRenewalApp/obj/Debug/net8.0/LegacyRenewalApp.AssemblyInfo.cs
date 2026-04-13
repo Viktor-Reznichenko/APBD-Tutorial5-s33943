e9eb390d3fe004d88435d385718e3393a760f145
s33943 <s33943@pjwstk.edu.pl>
2026-04-14 00:46:42 +0300

    Discount commit

--- LegacyRenewalApp/obj/Debug/net8.0/LegacyRenewalApp.AssemblyInfo.cs
+++ LegacyRenewalApp/obj/Debug/net8.0/LegacyRenewalApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LegacyRenewalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40949ec8ed0e335ae0c62c0ab3037a535613ebbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LegacyRenewalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LegacyRenewalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
