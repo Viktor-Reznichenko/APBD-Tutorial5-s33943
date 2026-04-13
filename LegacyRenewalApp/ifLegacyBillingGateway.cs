2af484a3fc17b11c0d8198fd6766a70f47594a37
s33943 <s33943@pjwstk.edu.pl>
2026-04-14 02:08:12 +0300

    Wrapper for LegacyBillingGateway

---
+++ LegacyRenewalApp/ifLegacyBillingGateway.cs
@@ -0,0 +1,7 @@
+﻿namespace LegacyRenewalApp;
+
+public interface ifLegacyBillingGateway
+{
+    void SaveInvoice(RenewalInvoice invoice);
+    void SendEmail(string email, string subject, string body);
+}
